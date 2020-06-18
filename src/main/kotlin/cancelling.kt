64245090b0075ca3dec8161646dda51839553848
Geovani <geovanimendoza.h@gmail.com>
2020-06-18 18:34:15 -0400

    fix: some imports

--- src/main/kotlin/cancelling.kt
+++ src/main/kotlin/cancelling.kt
@@ -1,7 +1,9 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.GlobalScope.coroutineContext
 
 fun main(args: Array<String>) = runBlocking{
     val job = GlobalScope.launch {
+        //coroutineContext[job]!!.isActive
         repeat(1000) {
             delay(100)
             print(".")
